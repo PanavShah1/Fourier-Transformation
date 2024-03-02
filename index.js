6236875f098447e35af45dec77c8001ae6049042
PanavShah1 <panav.shah@gmail.com>
2024-03-02 21:54:04 +0530

    done

--- index.js
+++ index.js
@@ -2,31 +2,60 @@ var elt = document.getElementById('calculator');
 var calculator = Desmos.GraphingCalculator(elt);
 
 // calculator.setExpression({id: 'circle', latex: 'x^2+y^2 < 1'})
-calculator.setExpression({id: 'm1', latex: 'y=x*x'})
+calculator.setExpression({id: 'm1', latex: 'y='})
 
 var state = calculator.getState().expressions.list[0].latex
 console.log(state)
+
+var function_ = calculator.getState().expressions.list[0].latex
 
 function userChange(){
     state = calculator.getState()
     console.log(state)
     // calculator.setExpression({id: 'm1', latex: 'y=\\sin(x)'})
     function_ = calculator.getState().expressions.list[0].latex
+
+    if (function_ != "y="){
+        document.getElementById("pointer").hidden = true
+    }
+    else{
+        document.getElementById("pointer").hidden = false
+    }
+
+    
+}
+// calculator.addEventListner("click", userChange)
+elt.addEventListener("click", userChange)
+document.addEventListener("keyup", userChange)
+
+
+function format(equation){
+    equation = equation.replaceAll("\\cos", "cos")
+    equation = equation.replaceAll("\\sin", "sin")
+    equation = equation.replaceAll("\\pi", "π")
+    equation = equation.replaceAll("+ -", "- ")
+    equation = equation.replaceAll("*", "")
+
+    return equation
+}
+
+function go(){
+    function_ = calculator.getState().expressions.list[0].latex
     console.log("function : "+function_)
     myPromise = api_call(function_)
 
     myPromise.then(
         function(value){
+            graph = value.graph
             console.log(value.graph)
-            calculator.setExpression({id: 'm2', latex: (value.graph)})
+            calculator.setExpression({id: 'm2', latex: (graph)})
+            document.getElementById("fourier-function").value = format(graph)
         },
         function(error){
             console.log(error)
         }
     )
 }
-// calculator.addEventListner("click", userChange)
-elt.addEventListener("click", userChange)
 // range_min = 1
 // range_max = 5
 // no_data_points = 10
@@ -43,6 +72,12 @@ function input_data(){
     console.log(range_min_, range_max_, no_data_points_)
 }
 document.addEventListener("input", input_data)
+
+
+
+
+
+
 
 async function api_call(function_) {
     const apiUrl = `http://127.0.0.1:8000/operate/`;
